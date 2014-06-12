583525085f0edf4587888f6265a0e7753e8e6623
Maximilian Mayer <mayer.maximilian@googlemail.com>
2014-06-12 19:49:20 +0200

    recreated root folder with rails 3 and checked out required files

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '41dc5811aec748aca5ab65025451ee9536531379a7e58bd0e0d02c1b3c2d572e001c1b3a8315371084f2c4520531611d4f2e4c85a963ced66adde810c18ec6b5';
--- test/unit/helpers/ipaddresses_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class IpaddressesHelperTest < ActionView::TestCase
-end
