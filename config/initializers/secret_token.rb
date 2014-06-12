39fd6b135a8e7cc209ffeadf40814bd2afc82a37
Maximilian Mayer <mayer.maximilian@googlemail.com>
2014-06-12 19:41:12 +0200

    recreated root folder with rails 3 and checked out required files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iplist::Application.config.secret_token = 'f9b8bdbd19d84b173b98ee98bad8cc080e71158724d5d9a466fdcd36e6f0978ef7631fa74051089dbcafd0dc60514d1588ddead69456a6625af866efb227311d'
